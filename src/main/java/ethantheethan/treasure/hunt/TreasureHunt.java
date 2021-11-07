5b439c680054b2b8b90b4f46858429e70d554ccc
Ethan <55063765+ethan-the-ethan@users.noreply.github.com>
2021-11-07 11:20:18 +0000

    Added new items + stuff

--- src/main/java/ethantheethan/treasure/hunt/TreasureHunt.java
+++ src/main/java/ethantheethan/treasure/hunt/TreasureHunt.java
@@ -1,10 +1,21 @@
 package ethantheethan.treasure.hunt;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,13 +26,96 @@ public class TreasureHunt implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("treasurehunt");
 
-	public static final Item PIECE_ONE = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item PIECE_TWO = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item PIECE_THREE = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item PIECE_FOUR = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item PIECE_FIVE = new Item(new Item.Settings().group(ItemGroup.MISC));
-	public static final Item PIECE_SIX = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
+		new Identifier("treasurehunt", "treasurehunt"))
+		.icon(() -> new ItemStack(TreasureHunt.PIECE_ONE))
+		.build();
 
+	public static class PieceOne extends Item {
+		public PieceOne(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_one.tooltip").formatted(Formatting.RED) );
+		}
+	}
+
+	public static class PieceTwo extends Item {
+		public PieceTwo(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_two.tooltip").formatted(Formatting.BLUE) );
+		}
+	}
+
+	public static class PieceThree extends Item {
+		public PieceThree(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_three.tooltip").formatted(Formatting.GREEN) );
+		}
+	}
+
+	public static class PieceFour extends Item {
+		public PieceFour(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_four.tooltip").formatted(Formatting.YELLOW) );
+		}
+	}
+
+	public static class PieceFive extends Item {
+		public PieceFive(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_five.tooltip").formatted(Formatting.GOLD) );
+		}
+	}
+
+	public static class PieceSix extends Item {
+		public PieceSix(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.piece_six.tooltip").formatted(Formatting.LIGHT_PURPLE).formatted(Formatting.OBFUSCATED));
+		}
+	}
+
+	public static class ColourWheel extends Item {
+		public ColourWheel(Settings settings) {
+			super(settings);
+		}
+		@Override
+		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+			// formatted red text
+			tooltip.add( new TranslatableText("item.treasurehunt.colour_wheel.tooltip").formatted(Formatting.AQUA).formatted(Formatting.ITALIC));
+		}
+	}
+
+	public static final PieceOne PIECE_ONE = new PieceOne(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.UNCOMMON));
+	public static final PieceTwo PIECE_TWO = new PieceTwo(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.UNCOMMON));
+	public static final PieceThree PIECE_THREE = new PieceThree(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.UNCOMMON));
+	public static final PieceFour PIECE_FOUR = new PieceFour(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.UNCOMMON));
+	public static final PieceFive PIECE_FIVE = new PieceFive(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.RARE));
+	public static final PieceSix PIECE_SIX = new PieceSix(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.RARE));
+	public static final ColourWheel COLOUR_WHEEL = new ColourWheel(new FabricItemSettings().group(TreasureHunt.ITEM_GROUP).maxCount(1).rarity(Rarity.EPIC));
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -29,12 +123,12 @@ public class TreasureHunt implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-
 		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_one"), PIECE_ONE);
 		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_two"), PIECE_TWO);
 		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_three"), PIECE_THREE);
 		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_four"), PIECE_FOUR);
 		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_five"), PIECE_FIVE);
-		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_six"), PIECE_SIX);	
+		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_six"), PIECE_SIX);
+		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "colour_wheel"), COLOUR_WHEEL);
 	}
 }

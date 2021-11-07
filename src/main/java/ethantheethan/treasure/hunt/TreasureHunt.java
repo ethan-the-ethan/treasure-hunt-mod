7561bb1ea52989a3b65af3fe07b83dca2bb58f6e
Ethan <55063765+ethan-the-ethan@users.noreply.github.com>
2021-11-07 09:10:04 +0000

    Added two items + dodgy placeholder art

--- src/main/java/ethantheethan/treasure/hunt/TreasureHunt.java
+++ src/main/java/ethantheethan/treasure/hunt/TreasureHunt.java
@@ -1,6 +1,11 @@
 package ethantheethan.treasure.hunt;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,7 +13,10 @@ public class TreasureHunt implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("treasurehunt");
+
+	public static final Item PIECE_ONE = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final Item PIECE_TWO = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
@@ -17,5 +25,8 @@ public class TreasureHunt implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_one"), PIECE_ONE);
+		Registry.register(Registry.ITEM, new Identifier("treasurehunt", "piece_two"), PIECE_TWO);
 	}
 }
